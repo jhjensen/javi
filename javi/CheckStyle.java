41ad1e979a8321888de13e77d0d96c75304620df
U-CARRIER\utftbwx <utftbwx@AE437358.carrier.utc.com>
2010-07-16 22:16:37 -0400

    fix checkstyle reader to pick up character offset
    cstyle cleanup

--- javi/CheckStyle.java
+++ javi/CheckStyle.java
@@ -1,9 +1,6 @@
 package javi;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,8 +18,8 @@ class CheckStyle extends Rgroup {
       register(rnames);
    }
 
-   public Object doroutine(int rnum,Object arg,int count,int rcount,
-         FvContext fvc, boolean dotmode) throws IOException,InputException {
+   public Object doroutine(int rnum, Object arg, int count, int rcount,
+         FvContext fvc, boolean dotmode) throws IOException, InputException {
       //trace("vigroup doroutine rnum = " + rnum );
       switch (rnum) {
       case 1:
@@ -36,18 +33,18 @@ class CheckStyle extends Rgroup {
       }
    }
 
-   static List<String>  listModFiles(String spec,FvContext fvc)
-         throws InputException,IOException {
+   static List<String>  listModFiles(String spec, FvContext fvc)
+         throws InputException, IOException {
 
       ArrayList<EditContainer> efs = FileList.writeModifiedFiles(spec);
 
       int count = efs.size();
       LinkedList<String> flist = new LinkedList<String>();
 
-      if (count ==0 && fvc!=null)  {
+      if (count == 0 && fvc != null)  {
          flist.add(fvc.edvec.getName());
       } else {
-         for (EditContainer ef:efs)
+         for (EditContainer ef : efs)
             flist.add(ef.getName());
       }
 
@@ -58,30 +55,30 @@ class CheckStyle extends Rgroup {
 
       try {
          List<String> cmd =
-            new LinkedList<String>(listModFiles(".*\\.java",fvc));
+            new LinkedList<String>(listModFiles(".*\\.java", fvc));
 
-         cmd.add(0,"../cstyle");
-         cmd.add(0,"bash");
+         cmd.add(0, "../cstyle");
+         cmd.add(0, "bash");
          PosListList.Cmd.setErrors(new CheckStyleInst(cmd));
       } catch (InputException e) {
-         throw new RuntimeException("cccommand has bad spec",e);
+         throw new RuntimeException("cccommand has bad spec", e);
       }
    }
 
-   private void cstylea() throws IOException,InputException {
+   private void cstylea() throws IOException, InputException {
       FileList.writeModifiedFiles(".*\\.java"); // write out java files
       String[] dlist =
-         FileDescriptor.LocalFile.cwdlist(new GrepFilter(".*\\.java$",false));
+         FileDescriptor.LocalFile.cwdlist(new GrepFilter(".*\\.java$", false));
 
       //trace("dlist = " + dlist);
-      if (dlist.length==0)
+      if (dlist.length == 0)
          UI.reportMessage("no files to compile");
       else  {
          List<String> cmd =
             new LinkedList<String>(java.util.Arrays.asList(dlist));
 
-         cmd.add(0,"../cstyle");
-         cmd.add(0,"bash");
+         cmd.add(0, "../cstyle");
+         cmd.add(0, "bash");
          PosListList.Cmd.setErrors(new CheckStyleInst(cmd));
       }
    }
@@ -90,47 +87,58 @@ class CheckStyle extends Rgroup {
 class CheckStyleInst extends PositionIoc {
 
    CheckStyleInst(List<String> filename) throws IOException {
-      super("checkstyle",Tools.runcmd (filename));
+      super("checkstyle", Tools.runcmd(filename));
    }
 
    public Position parsefile(String line) {
-      if (line.length()==0)
+      if (line.length() == 0)
          return Position.badpos;
 
-      trace("parsing len =  " + line.length() + " line: "  +line);
+      trace("parsing len =  " + line.length() + " line: "  + line);
 
-      int pos = line.indexOf(':',3); // three skips over any drive desc
+      int pos = line.indexOf(':', 3); // three skips over any drive desc
 
-      if (pos==-1) {
+      if (pos == -1) {
          //if (line.equals("Audit done.")
          //   ||  line.equals("Starting audit..."))
          //   return null;
-         trace("unexpected line:" +line);
+         trace("unexpected line:" + line);
          return Position.badpos;
       }
 
-      String file = line.substring(0,pos);
+      String file = line.substring(0, pos);
 
-      if (file.startsWith("javi/" ))
-         file=file.substring(5,pos);
+      if (file.startsWith("javi/"))
+         file = file.substring(5, pos);
 
-      line = line.substring(pos+1,line.length());
+      line = line.substring(pos + 1, line.length());
       pos = line.indexOf(':');
 
       if (pos == -1)
-         return new Position(0,0,file,line);
+         return new Position(0, 0, file, line);
 
       try {
-         int lineno = Integer.parseInt(line.substring(0,pos).trim());
+         int lineno = Integer.parseInt(line.substring(0, pos).trim());
 
-         if (lineno<=0)
-            lineno=1;
+         if (lineno <= 0)
+            lineno = 1;
 
-         String comment= line.substring(pos+1,line.length());
-         //trace(" returning " + new Position(0,lineno,file,comment));
-         return new Position(0,lineno,file,comment);
-      } catch ( Exception e) {
-         return new Position(0,0,file,line);
+         line = line.substring(pos + 1, line.length());
+         pos = line.indexOf(':');
+
+         if (pos == -1)
+            return new Position(0, lineno, file, line);
+
+         int charno = Integer.parseInt(line.substring(0, pos).trim()) - 1;
+
+         line = line.substring(pos + 1, line.length());
+         if (charno <= 0)
+            charno = 0;
+
+         //trace(" returning " + new Position(0, lineno, file, line));
+         return new Position(charno, lineno, file, line);
+      } catch (Exception e) {
+         return new Position(0, 0, file, line);
       }
    }
 }
