c683d6095ec11a7c37cbb119cb08d5842054a556
U-CARRIER\utftbwx <utftbwx@AE437358.carrier.utc.com>
2010-07-18 21:24:16 -0400

    cstyle changes

--- javi/BufInIoc.java
+++ javi/BufInIoc.java
@@ -1,6 +1,5 @@
-
 package javi;
- 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
@@ -9,26 +8,26 @@ import java.io.IOException;
 */
 
 abstract class BufInIoc<OType>  extends IoConverter<OType>  {
-transient public BufferedReader  input;
+   protected transient BufferedReader  input;
 
-final static String copyright = "Copyright 1996 James Jensen";
+   static final String copyright = "Copyright 1996 James Jensen";
 
-BufInIoc(FileProperties fp,boolean initThread) {
-   super(fp,initThread);
-}
+   BufInIoc(FileProperties fp, boolean initThread) {
+      super(fp, initThread);
+   }
 
-public void dispose() throws IOException {
-   super.dispose();
-   if (input !=null)
-      input.close();
-}
+   public void dispose() throws IOException {
+      super.dispose();
+      if (input != null)
+         input.close();
+   }
 
-EditCache<OType> convertStream(BufferedReader inputi)
-      throws ReadOnlyException,IOException {
-  //trace("index = " + index + " " + " input = " + inputi);
-  input=inputi;
- 
-  return super.convertStream();
-}
+   EditCache<OType> convertStream(BufferedReader inputi) throws
+         IOException {
+      //trace("index = " + index + " " + " input = " + inputi);
+      input = inputi;
+
+      return super.convertStream();
+   }
 
 }
