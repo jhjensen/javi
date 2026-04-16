cf8fa802e002adbd1b5730101e9532950889fa3f
jhjensen <jjensen@akamai.com>
2026-04-15 21:12:20 -0400

    T1: raise JaCoCo coverage thresholds

--- src/test/java/javi/MiscCommandsJUnitTest.java
+++ src/test/java/javi/MiscCommandsJUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
