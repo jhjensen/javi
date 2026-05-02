6b4212e4e721e71e8aa1c4e78dc66963cdf02f73
jhjensen <jjensen@akamai.com>
2026-05-02 07:06:08 -0400

    F37: Exit insert mode when F-key switches view

--- src/main/java/javi/InsertBuffer.java
+++ src/main/java/javi/InsertBuffer.java
@@ -331,6 +331,8 @@ public abstract class InsertBuffer extends View.Inserter {
                   if (key == JeyEvent.CHAR_UNDEFINED) {
                      itext(count, fvc);
                      MapEvent.hevent(ke, fvc);
+                     if (FvContext.getCurrFvc() != fvc)
+                        break;
                   } else if (verbatim && (key >= '0' && key <= '9')) {
                      verbatimCount++;
                      verbatimAcc = verbatimAcc * 10 + (key - '0');
